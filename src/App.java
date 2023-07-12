e02cebd07b80b0000b4b9d7773875a33c1e7b2ae
Firmanrachmad <dragobniel@gmail.com>
2023-07-12 21:14:22 +0700

    finish assignment

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Declare Variables
+        int num[];
+        int total = 0;
+        num = new int [4];
+        Scanner sc = new Scanner(System.in);
+        
+        // Input Numbers
+        for (int i=0; i < 4; i++) {
+            System.out.print("Input Numbers: ");
+            num[i] = sc.nextInt();
+        }
+        
+        // Sum Numbers
+        
+        for (int i=0; i < 4; i++) {
+            total += num[i];
+        }
+        
+        // View Inputted Numbers
+        for (int i=0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
+        System.out.print("Total = " + total);
     }
 }
